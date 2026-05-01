617ff654ed589a1d1462685f347e5250ccf23745
Spoons-III <twgeary@uvm.edu>
2026-04-30 22:49:41 -0400

    Finished array_max.asm

--- asm/array_max.asm
+++ asm/array_max.asm
@@ -19,18 +19,36 @@ SETUP:
     ; memory addresses in a loop.
     STORE R1, [R6 + #0]   ; M[R6] = value
     ; Complete loop body here
+    ADDI R1, R1, #1
+    ADDI R4, R4, #1
+    ADDI R6, R6, #1
+    SUB R5, R4, R2
     BLT   SETUP           ; if counter < N, continue
 
-    ; reset counter
-    ; reset address to base
+    LOADI R4, #0          ; reset counter
+    MOV R6, R0            ; reset address to base
     LOAD  R3, [R6 + #0]   ; max = M[0]  (seed with first element)
-    ; advance past first element (just increment)
+    ADDI R6, R6, #1       ; advance past first element (just increment)
     LOADI R4, #1          ; counter = 1  (first element already processed)
 
-; Complete the loop which finds the largest value and stores
-; the result in R3. Do this work in the loop. Even though we
-; know the largest value, show that you can write the assembly
-; code to do this.
+FINDLARGEST:
+    ; Complete the loop which finds the largest value and stores
+    ; the result in R3. Do this work in the loop. Even though we
+    ; know the largest value, show that you can write the assembly
+    ; code to do this.
+    LOAD R1, [R6 + #0]     ; Update curr
+    SUB R5, R3, R1         ; Check if curr > max
+    BLT UPDATELARGEST
+    B KEEPON               ; Not necessary, I know, but I like it
+KEEPON:
+    ADDI R6, R6, #1        ; Iterate memory address
+    ADDI R4, R4, #1        ; Iterate counter
+    SUB R5, R4, R2         ; check if NEG is set
+    BLT FINDLARGEST        ; contiune if so
+    B DONE                 ; stop if not
 
+UPDATELARGEST:
+    MOV R3, R1             ; New largest
+    B KEEPON               ; Contiune the loop
 DONE:
     HALT
