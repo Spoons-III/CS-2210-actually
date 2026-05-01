85bf492e2ff1f55ade418a9e44496bb6d67d7f4e
LeeEricson <l333ricson@gmail.com>
2026-05-01 19:48:52 -0400

    so far

--- asm/popcount.asm
+++ asm/popcount.asm
@@ -8,16 +8,16 @@
     LOADI R1, #0x0F       ; test value (4 set bits)
     LOADI R2, #0          ; count = 0
     ; complete the problem setup here
-    LOAD R3, #0           ; counter = 0
-    LOAD R4, #16          ; limit = 16
-    LOAD R5, #0x1         ; LSB mask 
-    LOAD R6, #1           ; shift control (right)
+    LOADI R3, #0           ; counter = 0
+    LOADI R4, #16          ; limit = 16
+    LOADI R5, #0x1         ; LSB mask 
+    LOADI R6, #1           ; shift control (right)
 LOOP:
     ; complete loop
     AND R7, R1, R5
     ADD R3, R3, R7
     SHFT R1, R4, R6
     SUB R7, R4, R3
-    BLT   LOOP            ; if counter < 16, continue
+    BLT   LOOP             ; if counter < 16, continue
 DONE:
     HALT
